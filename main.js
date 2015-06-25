9d1cf3edf26a529eb8a5fdd06c1260f99fa0fb14
anmcintyre <andrea.mcintyre@gmail.com>
2015-06-25 16:43:35 -0400

    added validation

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
-angular.module('instagramSearcher', [])
+angular.module('instagramSearcher', ['ngMessages', 'ngAnimate'])
 .controller('MyController', function($scope, $http) {
   $scope.photos = [];
-  $scope.submit = function() {
+  $scope.count = -1;
+  $scope.lastSearch = "";
+  $scope.httpError = false;
+
+  $scope.submit = function(form) {
+    if ( $scope.text.trim().length === 0){
+      $scope.photos = [];  
+      $scope.count = -1;  
+      $scope.lastSearch = "";
+      $scope.httpError = false;
+      return;
+    }
+
     var request = {
       client_id: '09fe30ae878a416f819cc4e0db3db363',
       callback: "JSON_CALLBACK"
     };
+    console.log()    
 
     $http({
       method: 'JSONP',
-      url: "https://api.instagram.com/v1/tags/" + $scope.text + "/media/recent",
+      url: "https://api.instagram.com/v1/tags/" + $scope.text.replace(/ /g, "") + "/media/recent",
       params: request
     })
     .success(function(response, status, headers, config) { 
       $scope.photos = response.data;
+      $scope.count = response.data.length;
+      $scope.lastSearch = $scope.text;
+      $scope.text = "";
+      $scope.myForm.$setPristine();   
+      $scope.httpError = false;   
     })
     .error(function(data, status, headers, config) {
-      alert('error');
+      $scope.httpError = true;
+      $scope.status = status;
+      $scope.url = config.url;
+      $scope.text = "";      
+      $scope.myForm.$setPristine();
     });
   };
 }); 
